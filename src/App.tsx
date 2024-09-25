911bc406ae66522a6f288bd824c72134d46359a2
Yimura <24669514+Yimura@users.noreply.github.com>
2024-09-25 12:26:02 +0000

    feat(App): make StatusBar visible

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,19 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import BottomNavigator from 'components/BottomNavigator';
+import { StatusBar } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name='root' component={BottomNavigator} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <>
+      <StatusBar animated={true} barStyle={'dark-content'} backgroundColor={'#0000'} />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen name='root' component={BottomNavigator} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
   )
 };
