1e9fb389b4c29d1ab853d827c8139e804f22b7aa
Yimura <24669514+Yimura@users.noreply.github.com>
2025-04-28 22:12:39 +0000

    feat(IconButton): add textStyle

--- frontend/components/core/IconButton.tsx
+++ frontend/components/core/IconButton.tsx
@@ -1,22 +1,22 @@
 import React, { ReactElement } from 'react';
-import { PressableProps, Text, ViewStyle } from 'react-native';
+import { PressableProps, Text, TextStyle } from 'react-native';
 import { Button } from './Button';
 
 
 interface IconButtonProps extends PressableProps {
     icon: ReactElement;
     label?: string;
-    style?: ViewStyle;
+    textStyle?: TextStyle;
     onPress?: () => void;
     onLongPress?: () => void;
 }
 
-export function IconButton({ icon, label, style, onPress, onLongPress }: IconButtonProps): React.JSX.Element {
+export function IconButton({ icon, label, style, textStyle, onPress, onLongPress }: IconButtonProps): React.JSX.Element {
 
     return (
         <Button style={style} onPress={onPress} onLongPress={onLongPress}>
             {icon}
-            {label && <Text>{label}</Text>}
+            {label && <Text style={textStyle}>{label}</Text>}
         </Button>
     );
 }
