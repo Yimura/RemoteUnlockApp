1ed0cc36b0f92aff2fd2fe812b7222886f3410e0
Yimura <24669514+Yimura@users.noreply.github.com>
2025-04-28 11:06:18 +0000

    feat(VehiclesPage): add list support

--- frontend/pages/MyVehiclesPage.tsx
+++ frontend/pages/MyVehiclesPage.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { VehicleCard } from '../components/VehicleCard';
+import { FlatList, StyleSheet, View } from 'react-native';
+import { VehicleCard, VehicleCardProps } from '../components/VehicleCard';
 
 
 export function MyVehiclesPage(): React.JSX.Element {
+    const vehicles: VehicleCardProps['vehicle'][] = [
+        { model: 'BMW E36', battery: 14.4, connected: true, locked: true, lastConnected: new Date(Date.now() - 6e5) },
+        { model: 'Tesla Model 3', battery: 11.6, connected: true, locked: false, lastConnected: new Date(Date.now() - 3e5) },
+        { model: 'Defender 90', battery: 10.3, connected: false, locked: true, lastConnected: new Date(Date.now() - 1e6) },
+    ];
+
     return (
         <View style={styles.container}>
-            <VehicleCard model={'BMW E36'} battery={12.4} connected={true} locked={true} lastConnected={new Date(Date.now() - 6e5)} />
+            <FlatList data={vehicles} renderItem={({ item }) => <VehicleCard vehicle={item} style={styles.item} />} keyExtractor={item => item.model} />
         </View>
     );
 }
@@ -14,6 +20,9 @@ export function MyVehiclesPage(): React.JSX.Element {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 16,
+    },
+    item: {
+        marginHorizontal: 16,
+        marginVertical: 8,
     },
 });
