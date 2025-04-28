6040b74dbb5f5e404674bd07e532f09b64f4498a
Yimura <24669514+Yimura@users.noreply.github.com>
2025-04-28 13:31:27 +0000

    feat(SettingsPage): create basic design

--- frontend/pages/SettingsPage.tsx
+++ frontend/pages/SettingsPage.tsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import { StyleSheet, Switch, SwitchProps, Text, View } from 'react-native';
+import { Card } from '../components/core/Card';
+import { Blue, Grey, OffBlue, OffWhite, White } from '../theme/Color';
+import { Button } from '../components/core/Button';
+
+interface SettingItemProps {
+    label: string;
+    description: string;
+    onChange?: SwitchProps['onChange'];
+    value?: boolean;
+}
+
+const SettingItem = ({ label, description, onChange, value }: SettingItemProps): React.JSX.Element => {
+    return (
+        <View style={styles.settingItem}>
+            <View style={styles.settingItemText}>
+                <Text>{label}</Text>
+                <Text style={styles.description}>{description}</Text>
+            </View>
+            <View>
+                <Switch trackColor={{
+                    true: Blue,
+                    false: Grey,
+                }} thumbColor={White} onChange={onChange} value={value} />
+            </View>
+        </View>
+    );
+};
 
 export function SettingsPage(): React.JSX.Element {
     return (
-        <></>
+        <View style={styles.container}>
+            <Card style={styles.card}>
+                <View>
+                    <Text style={styles.title}>App Settings</Text>
+                    <Text style={styles.description}>Configure how the application behaves.</Text>
+                </View>
+                <View style={styles.settingsItems}>
+                    <SettingItem label="Run in Background" description="Allow the app to run in the backgrund for auto-lock features." value={true} />
+                    <SettingItem label="Notifications" description="Receive notifications when your vehicle is locked or unlocked" value={false} />
+                </View>
+                <View>
+                    <Button style={({ pressed }) => pressed ? { backgroundColor: OffBlue } : { backgroundColor: Blue }}>
+                        <Text style={{ color: OffWhite }}>Save Changes</Text>
+                    </Button>
+                </View>
+            </Card >
+        </View >
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 16,
+    },
+    card: {
+        gap: 16,
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 600,
+    },
+    description: {
+        fontSize: 12,
+        color: Grey,
+    },
+    settingsItems: {
+        gap: 8,
+    },
+    settingItem: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        gap: 8,
+    },
+    settingItemText: {
+        flex: 1,
+    },
+});
--- frontend/theme/Color.ts
+++ frontend/theme/Color.ts
@@ -1,7 +1,8 @@
-export const Blue: string = 'rgb(37 99 235)';
-export const Green: string = 'rgb(22 163 74)';
-export const Orange: string = 'rgb(217 119 6)';
-export const Red: string = 'rgb(239 68 68)';
+export const Blue: string = 'rgb(37, 99, 235)';
+export const OffBlue: string = 'rgb(68, 122, 238)';
+export const Green: string = 'rgb(22, 163, 74)';
+export const Orange: string = 'rgb(217, 119, 6)';
+export const Red: string = 'rgb(239, 68, 68)';
 
 export const Black: string = '#000';
 export const Grey: string = '#888';
