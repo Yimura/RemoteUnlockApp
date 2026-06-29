b609b5567d1208e6512342c61a6b776812f6c539
Yimura <24669514+Yimura@users.noreply.github.com>
2026-06-29 23:19:05 +0200

    feat(pairing): use brand logo in Welcome screen

--- frontend/features/pairing/pages/Welcome.tsx
+++ frontend/features/pairing/pages/Welcome.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Bluetooth } from 'lucide-react-native';
+import Logo from '@/assets/brand/logo.svg';
 import { PairContainer } from '../components/PairContainer';
 import { usePaginator } from '../components/paginator';
 
@@ -12,7 +12,7 @@ export function Welcome(): React.JSX.Element {
 
     return (
         <PairContainer>
-            <PairContainer.Icon IconComponent={Bluetooth} />
+            <Logo width={96} height={96} />
             <PairContainer.Title text="Device Bluetooth Pairing" />
             <PairContainer.SubTitle text="We'll guide you through the process of connecting your Bluetooth device to your mobile device. Make sure your device is turned on and nearby." />
         </PairContainer>
